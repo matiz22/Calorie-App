93ecc802b57142a282a6e64d4959249d28a67904
matiz22 <mateuszmalich@gmail.com>
2023-04-24 20:05:28 +0200

    request notification

--- app/src/main/java/com/example/calorieapp/MainActivity.kt
+++ app/src/main/java/com/example/calorieapp/MainActivity.kt
@@ -20,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.calorieapp.navigation.CalorieNavigation
@@ -35,6 +36,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            if (!(ContextCompat.checkSelfPermission(
+                    applicationContext,
+                    Manifest.permission.SEND_SMS
+                ) == PackageManager.PERMISSION_GRANTED)
+            ){
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
             CalorieAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
